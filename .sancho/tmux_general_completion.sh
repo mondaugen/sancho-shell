295dad4118010fc302e85223543cbd7244d5f0c5
Nicholas Esterer <nicholas.esterer@plot.ly>
2021-12-16 18:21:09 -0500

    complete paths with any substring, anywhere
    Type <tmux prefix>* and get a list of paths that will be inserted if you
    select one.
    
    NOTE: To reduce the number of paths, it only lists paths that are not
    ignored by git, so currently it is meant to be used in repos.
    
    TODO: Right now if too many paths are generated, it won't show any
    pop-up. We should make it so it just cuts off the number of paths so as
    many as possible get displayed.

---
+++ .sancho/tmux_general_completion.sh
@@ -0,0 +1,64 @@
+get_word_under_cursor ()
+{
+    tmux capturep -p | python3 -c '
+import sys
+import re
+matcher=re.compile("\S")
+# x, y position read from stdin
+lines = sys.stdin.readlines()
+y=min(int(sys.argv[2]),len(lines)-1)
+line=lines[y]
+x=min(int(sys.argv[1]),len(line)-1)
+def word_begin(line,x):
+    while x > 0 and matcher.match(line[x-1]):
+        x -= 1
+    return x
+begin = word_begin(line,x)
+word = line[begin:x]
+print(word)' $1 $2
+}
+
+# The argument is a string (or regular expression) that will match anywhere in
+# path names tracked by git
+generate_path_matches ()
+{
+    lsfiles='git ls-files -co' #| grep
+    if [ -a .gitignore ]; then
+        lsfiles="${lsfiles}X .gitignore"
+    fi
+    $lsfiles |grep "$1"
+}
+
+complete_path_under_cursor ()
+{
+    generate_path_matches $(get_word_under_cursor $1 $2)
+}
+
+word_length_under_cursor ()
+{
+    python3 -c 'import sys; print(len(sys.argv[1]))' $(get_word_under_cursor $1 $2)
+}
+
+delete_word_under_cursor ()
+{
+    wlen=$(word_length_under_cursor $1 $2)
+    tmux send-keys -N $wlen 
+}
+
+make_tmux_menu ()
+{
+    # take a list of files, separated by newlines, on stdin
+    # interleave them with a key to select that menu item and a command to run that will insert it
+    wlen=$(word_length_under_cursor $1 $2)
+    x=$(complete_path_under_cursor $1 $2 | python3 -c '
+import sys
+import string
+keys=string.digits[1:]+string.ascii_letters.replace("q","").replace("Q","")
+lines=sys.stdin.readlines()
+wlen=int(sys.argv[1])
+print("tmux display-menu "+" ".join([f"{line.strip()} {key} \"send-keys -N {wlen}  ; send-keys {line.strip()}\"" for line,key in zip(lines,keys)]))
+' $wlen)
+    eval "$x"
+}
+
+make_tmux_menu $1 $2
