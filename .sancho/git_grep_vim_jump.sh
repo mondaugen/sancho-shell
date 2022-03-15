27e7ca7a1f198f8025dcfb8d1d467a5a962f7ac2
Nicholas Esterer <ner@pixmob.com>
2022-03-15 15:07:24 -0400

    git grep for things with spaces

--- .sancho/git_grep_vim_jump.sh
+++ .sancho/git_grep_vim_jump.sh
@@ -1,3 +1,3 @@
 cheight=$(tmux display-message -p '#{client_height}')
-eval "$(git grep --recurse-submodules -I -n -r $1|PYTHONPATH=~/.sancho:$PYTHONPATH python3 ~/.sancho/git_grep_vim_jump.py $cheight)"
+eval "$(git grep --recurse-submodules -I -n -r "$1"|PYTHONPATH=~/.sancho:$PYTHONPATH python3 ~/.sancho/git_grep_vim_jump.py $cheight)"
 
