6005a3d6487a8202c114b090cbcfc6528c0ed35f
ner <ner@pixmob.com>
2022-11-11 12:36:37 -0500

    path completion (prefix *) in other directories

--- .sancho/tmux_general_completion.sh
+++ .sancho/tmux_general_completion.sh
@@ -18,16 +18,28 @@ word = line[begin:x]
 print(word)' $1 $2
 }
 
-# The argument is a string (or regular expression) that will match anywhere in
+# $1 is a string (or regular expression) that will match anywhere in
 # path names tracked by git
+# $2 is the root folder where searches will be carried out from
 generate_path_matches ()
 {   
-    git grep --recurse-submodules -l -I -n -r -e '.*' | grep "$1"
+    git -C $2 grep --exclude-standard --no-index -l -I -n -r -e '.*' | grep "$1" | sed 's|^|'$2'/&|'
 }
+
+get_dirname_under_cursor ()
+{
+    word=$(get_word_under_cursor $1 $2)
+    word_dirname=$(dirname $word | sed 's|^~|'$HOME'|')
+    echo $word_dirname
+}
+    
 
 complete_path_under_cursor ()
 {
-    generate_path_matches $(get_word_under_cursor $1 $2)
+    word=$(get_word_under_cursor $1 $2)
+    word_dirname=$(get_dirname_under_cursor $1 $2)
+    word_basename=$(basename $word)
+    generate_path_matches $word_basename $word_dirname
 }
 
 word_length_under_cursor ()
@@ -48,6 +60,7 @@ make_tmux_menu ()
     cheight=$(tmux display-message -p '#{client_height}')
     cwidth=$(tmux display-message -p '#{client_width}')
     wlen=$(word_length_under_cursor $1 $2)
+    word_dirname=$(get_dirname_under_cursor $1 $2)
     x=$(complete_path_under_cursor $1 $2 | python3 -c '
 import sys
 import string
@@ -56,16 +69,17 @@ keys=string.digits[1:]+string.ascii_letters.replace("q","").replace("Q","").repl
 wlen=int(sys.argv[1])
 cheight=int(sys.argv[2])-2 # - 2 to allow menu border
 cwidth=int(sys.argv[3])-22 # - 2 to allow menu border
+len_word_dirname=int(len(sys.argv[4])) + 1 # To remove joining /
 lines=[l.replace(" ","\ ") for l in sys.stdin.readlines()]
 if len(lines) == 0:
     sys.exit(1)
 lines=lines[:min(len(lines),cheight)]
-lines=list(filter(lambda l: len(l.strip()) <= cwidth,lines))
+lines=list(filter(lambda l: len(l.strip()[len_word_dirname:]) <= cwidth,lines))
 def escape_space(s):
     return s.replace(" ","\\\\ Space ")
-tmux_cmd="tmux display-menu "+" ".join([f"{line.strip()} {key} \"send-keys -N {wlen}  ; send-keys {escape_space(line.strip())}\"" for line,key in zip(lines,keys)])
+tmux_cmd="tmux display-menu "+" ".join([f"{line.strip()[len_word_dirname:]} {key} \"send-keys -N {wlen}  ; send-keys {escape_space(line.strip())}\"" for line,key in zip(lines,keys)])
 print(tmux_cmd)
-' $wlen $cheight $cwidth)
+' $wlen $cheight $cwidth $word_dirname)
     eval "$x"
 }
 
