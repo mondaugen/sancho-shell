bb65fbc384a9ae41b929ff329794b78b12db1822
ner <ner@pixmob.com>
2022-11-11 10:21:46 -0500

    ,g* in vim now works outside of a repo
    and even in a repo it will search untracked files

--- .sancho/git_grep_vim_jump.sh
+++ .sancho/git_grep_vim_jump.sh
@@ -1,13 +1,2 @@
-grp ()
-{
-    grep -rnI --exclude-dir=.git --exclude-dir=venv "$@"
-}
-
 cheight=$(tmux display-message -p '#{client_height}')
-# TODO: It would be nice to search in the repo or folder that contains the
-# source file passed in as argument $2, but at that point it is hard to know
-# where to limit the search...
-#eval "$(grp "$1" "$(dirname $2)"|PYTHONPATH=~/.sancho:$PYTHONPATH python3 ~/.sancho/git_grep_vim_jump.py $cheight)"
-
-eval "$(git grep --recurse-submodules -I -n -r "$1"|PYTHONPATH=~/.sancho:$PYTHONPATH python3 ~/.sancho/git_grep_vim_jump.py $cheight)"
-
+eval "$(git grep --no-index --exclude-standard -I -n -r "$1"|PYTHONPATH=~/.sancho:$PYTHONPATH python3 ~/.sancho/git_grep_vim_jump.py $cheight)"
