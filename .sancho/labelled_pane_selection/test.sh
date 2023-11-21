c00ec7f3355ae10f45b932685129747d385ae5e3
ner <ner@pixmob.com>
2023-11-21 15:57:57 -0500

    The text on which to do the selection can come from copy mode
    The only requirement is that the cursor be on the top line, that way we
    can parse the history line number from the cursor line (I didn't find a
    better way to do this, maybe this number is obtainable directly
    somehow). So the cursor needs to be forced to the top if in copy mode
    before doing the capture.

--- .sancho/labelled_pane_selection/test.sh
+++ .sancho/labelled_pane_selection/test.sh
@@ -4,7 +4,15 @@ LOOPS=1
 do_capture ()
 {
     echo "$LOOPS" >> /tmp/loops
-    tmux capture-pane -pt$TARGET_PANE > /tmp/a
+    copy_cursor_line=$(tmux display -pt$TARGET_PANE '#{copy_cursor_line}')
+    if [ -z $copy_cursor_line ]; then
+        tmux capture-pane -pt$TARGET_PANE > /tmp/a
+    else
+        # find out where we are in the history
+        hist_line_num=$(tmux display -pt$TARGET_PANE '#{copy_cursor_line}' | sed -E 's!^[^[]+\[([0-9]+)/[0-9]+\]$!\1!g')
+        tmux capture-pane -S-$hist_line_num \
+            -E$(( $(tmux display -pt$TARGET_PANE '#{pane_height}') - $hist_line_num - 1 )) -pt$TARGET_PANE > /tmp/a
+    fi 
     rm -f /tmp/d
     tmux neww -eLOOPS=$LOOPS -eMW=$MW -eMH=$MH -eMATCHER_STYLE=$MATCHER_STYLE 'tmux set-option -w remain-on-exit off && python3 .sancho/labelled_pane_selection/region_selector.py 2>/tmp/region_selector_error && cat /tmp/b | head -n $(($(tput lines) - 0)) | tail -n $(($(tput lines) - 0))| head --bytes=-1 && read -sn 1 tempy && echo -n "$tempy" > /tmp/d'
     # wait for /tmp/d to show up because it seems tmux neww is asynchronous
