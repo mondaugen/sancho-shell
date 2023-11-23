c65d7f9d5d6b93d2620668b4a558083dfdc6f3f4
ner <ner@pixmob.com>
2023-11-23 00:04:41 -0500

    just a typo in the scope_matcher.py script

--- .sancho/labelled_pane_selection/scope_matcher.py
+++ .sancho/labelled_pane_selection/scope_matcher.py
@@ -2,8 +2,9 @@ import numpy as np
 
 def all_scope_depths(text,delims='{}'):
     scope_boundaries=np.zeros(len(text))
-    scope_boundaries[[i for i,_ in filter(lambda t: t[1] == delims[0],enumerate(s))]] = 1
-    scope_boundaries[[i for i,_ in filter(lambda t: t[1] == delims[1],enumerate(s))]] = -1
+    scope_boundaries[[i for i,_ in filter(lambda t: t[1] == delims[0],enumerate(text))]] = 1
+    scope_boundaries[[i for i,_ in filter(lambda t: t[1] == delims[1],enumerate(text))]] = -1
+    #print("".join(['%u' % (i,) for i in scope_boundaries]))
     scope_depths=np.cumsum(scope_boundaries)
     return scope_depths
 
@@ -14,6 +15,7 @@ def scopes_at_depth(scope_depths,d=1,):
     """
     scopes=np.zeros(len(scope_depths)+1)
     scopes[1:][scope_depths >= d] = 1
+    #print("".join(['%u' % (i,) for i in scopes[1:]]))
     scopes_diff=np.diff(scopes)
     for sta,sto in zip(np.where(scopes_diff>0)[0],np.where(scopes_diff<0)[0]):
         yield (sta,sto+1)
@@ -25,7 +27,9 @@ u=' { {{}} {} {  } {{ } } '
 for ss in [s,t,u]:
     print(ss)
     for dd in [1,2]:
+        scope_depths=all_scope_depths(ss)
         print("depth:",dd)
-        for sta,sto in scopes_at_depth(all_scope_depths(ss),d=dd):
+        print("".join(['%u' % (i,) for i in scope_depths]))
+        for sta,sto in scopes_at_depth(scope_depths,d=dd):
             print("".join([ss[:sta]]+["x" for _ in range(sta,sto)]+[ss[sto:]]))
     print()
