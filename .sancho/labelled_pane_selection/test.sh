a2ff3ff0a7a0f2ef5aac8bda55a14d30deffbb74
ner <ner@pixmob.com>
2023-12-13 12:01:15 -0500

    more specifically named tmp files

--- .sancho/labelled_pane_selection/test.sh
+++ .sancho/labelled_pane_selection/test.sh
@@ -11,12 +11,12 @@ do_move ()
         vim)
             c_s=$(tmux display -pt$TARGET_PANE '#{cursor_x}')
             l_s=$(tmux display -pt$TARGET_PANE '#{cursor_y}')
-            l_s_p=$(cat /tmp/c|awk '{print $1}')
-            c_s_p=$(cat /tmp/c|awk '{print $2}')
+            l_s_p=$(cat /tmp/labelled_pane_selection_c|awk '{print $1}')
+            c_s_p=$(cat /tmp/labelled_pane_selection_c|awk '{print $2}')
             tmux send-keys -t$TARGET_PANE Escape :call Space MovTextAbs\($c_s,$l_s,$c_s_p,$l_s_p\) Enter
             ;;
         *)
-            echo -ne '\e['$(( 1 + $(cat /tmp/c|awk '{print $1}') ))';'$(( 1 + $(cat /tmp/c|awk '{print $2}') ))'H' > \
+            echo -ne '\e['$(( 1 + $(cat /tmp/labelled_pane_selection_c|awk '{print $1}') ))';'$(( 1 + $(cat /tmp/labelled_pane_selection_c|awk '{print $2}') ))'H' > \
                 $(tmux display-message -pt$TARGET_PANE '#{pane_tty}')
     esac
 }
@@ -25,7 +25,7 @@ text_op ()
 {
     case $FINAL_MODE in
         select)
-            tmux load-buffer /tmp/c && echo
+            tmux load-buffer /tmp/labelled_pane_selection_c && echo
             ;;
         move_start)
             do_move
@@ -45,37 +45,37 @@ do_capture ()
     echo "$LOOPS" >> /tmp/loops
     copy_cursor_line=$(tmux display -pt$TARGET_PANE '#{copy_cursor_line}')
     if [[ -z $copy_cursor_line ]]; then
-        tmux capture-pane -pt$TARGET_PANE > /tmp/a
+        tmux capture-pane -pt$TARGET_PANE > /tmp/labelled_pane_selection_a
     else
         # find out where we are in the history
         hist_line_num=$(tmux display -pt$TARGET_PANE '#{copy_cursor_line}' | sed -E 's!^[^[]+\[([0-9]+)/[0-9]+\]$!\1!g')
         tmux capture-pane -S-$hist_line_num \
-            -E$(( $(tmux display -pt$TARGET_PANE '#{pane_height}') - $hist_line_num - 1 )) -pt$TARGET_PANE > /tmp/a
+            -E$(( $(tmux display -pt$TARGET_PANE '#{pane_height}') - $hist_line_num - 1 )) -pt$TARGET_PANE > /tmp/labelled_pane_selection_a
     fi 
-    rm -f /tmp/d
-    tmux neww -eLOOPS=$LOOPS -eMW=$MW -eMH=$MH -eMATCHER_STYLE=$MATCHER_STYLE 'tmux set-option -w remain-on-exit off && python3 .sancho/labelled_pane_selection/region_selector.py 2>/tmp/region_selector_error && cat /tmp/b | head -n $(($(tput lines) - 0)) | tail -n $(($(tput lines) - 0))| head --bytes=-1 && read -sn 1 tempy && echo -n "$tempy" > /tmp/d'
-    # wait for /tmp/d to show up because it seems tmux neww is asynchronous
+    rm -f /tmp/labelled_pane_selection_d
+    tmux neww -eLOOPS=$LOOPS -eMW=$MW -eMH=$MH -eMATCHER_STYLE=$MATCHER_STYLE 'tmux set-option -w remain-on-exit off && INFILE=/tmp/labelled_pane_selection_a OUTFILE=/tmp/labelled_pane_selection_b python3 .sancho/labelled_pane_selection/region_selector.py 2>/tmp/region_selector_error && cat /tmp/labelled_pane_selection_b | head -n $(($(tput lines) - 0)) | tail -n $(($(tput lines) - 0))| head --bytes=-1 && read -sn 1 tempy && echo -n "$tempy" > /tmp/labelled_pane_selection_d'
+    # wait for /tmp/labelled_pane_selection_d to show up because it seems tmux neww is asynchronous
     while [ 1 ]
     do
-        if [ -f /tmp/d ]
+        if [ -f /tmp/labelled_pane_selection_d ]
         then
             break
         fi
         sleep 0.1
     done
-    cat /tmp/d | MODE=$FINAL_MODE python3 .sancho/labelled_pane_selection/region_selector.py 2>/tmp/final_selector_error > /tmp/c
+    cat /tmp/labelled_pane_selection_d | MODE=$FINAL_MODE INFILE=/tmp/labelled_pane_selection_a python3 .sancho/labelled_pane_selection/region_selector.py 2>/tmp/final_selector_error > /tmp/labelled_pane_selection_c
     case $? in
         0)
             text_op && echo
             ;;
         1)
             echo "got error $?"
-            cat /tmp/d
+            cat /tmp/labelled_pane_selection_d
             LOOPS=$(( $LOOPS + 1 )) do_capture
             ;;
         2)
             echo "got error $?"
-            cat /tmp/d
+            cat /tmp/labelled_pane_selection_d
             LOOPS=$(( $LOOPS - 1 )) do_capture
             ;;
         *)
